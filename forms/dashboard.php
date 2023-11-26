57c2eddc2c8d093e1db7683309e52d362bfefef1
ilhanabhit <ilhan.abhitah86@gmail.com>
2023-11-26 18:43:11 +0700

    baru

--- forms/dashboard.php
+++ forms/dashboard.php
@@ -23,7 +23,7 @@
 
 </head>
 
-<body style="height: max-content;">
+<body style="height: max-content; width: 100%;">
     <div class="wrapper d-flex align-items-stretch">
         <!-- navbar -->
         <?php include 'navbar.php'; ?>
